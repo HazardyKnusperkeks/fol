88dbc216a671f1e86fbba5cd75ecb3088dcdf170
Björn Schäpers <bjoern@hazardy.de>
2017-08-01 20:50:42 +0200

    Function: Add comparison operators

--- function.hpp
+++ function.hpp
@@ -30,7 +30,23 @@ class Function {
 		} //if constexpr ( sizeof...(Args) >= 1 )
 		return os;
 	}
+	
+	template<typename Name1T, typename Name2T, typename... Args1, typename... Args2>
+	friend constexpr bool operator==(const Function<Name1T, Args1...> f1, const Function<Name2T, Args2...> f2) noexcept;
+	
+	template<typename Name1T, typename Name2T, typename... Args1, typename... Args2>
+	friend constexpr bool operator!=(const Function<Name1T, Args1...> f1, const Function<Name2T, Args2...> f2) noexcept;
 };
+
+template<typename Name1T, typename Name2T, typename... Args1, typename... Args2>
+constexpr bool operator==(const Function<Name1T, Args1...> f1, const Function<Name2T, Args2...> f2) noexcept {
+	return f1.N == f2.N && f1.A == f2.A;
+}
+
+template<typename Name1T, typename Name2T, typename... Args1, typename... Args2>
+constexpr bool operator!=(const Function<Name1T, Args1...> f1, const Function<Name2T, Args2...> f2) noexcept {
+	return !(f1 == f2);
+}
 
 }
 
