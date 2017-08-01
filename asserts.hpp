c15a33faa75e47bede66c2884061a5af9b58559d
Björn Schäpers <bjoern@hazardy.de>
2017-08-01 13:41:05 +0200

    Align asserts

--- asserts.hpp
+++ asserts.hpp
@@ -10,18 +10,20 @@
 
 namespace fol {
 
-static_assert(Variable<'x'>{} == Variable<'x'>{});
+static_assert(Variable<'x'>{}   == Variable<'x'>{});
 static_assert(!(Variable<'x'>{} != Variable<'x'>{}));
-static_assert(Variable<'x'>{} != Variable<'y'>{});
+static_assert(Variable<'x'>{}   != Variable<'y'>{});
 static_assert(!(Variable<'x'>{} == Variable<'y'>{}));
 
-static_assert(Variable<'x'>{}.prev() == Variable<'w'>{});
-static_assert(Variable<'x', 'b'>{}.prev() == Variable<'x', 'a'>{});
-static_assert(Variable<'x', 'a'>{}.prev() == Variable<'w', 'z'>{});
+static_assert(Variable<'x'>{}.prev()           == Variable<'w'>{});
+static_assert(Variable<'x', 'b'>{}.prev()      == Variable<'x', 'a'>{});
+static_assert(Variable<'x', 'a'>{}.prev()      == Variable<'w', 'z'>{});
 static_assert(Variable<'x', 'b', 'c'>{}.prev() == Variable<'x', 'b', 'b'>{});
 static_assert(Variable<'x', 'y', 'a'>{}.prev() == Variable<'x', 'x', 'z'>{});
-static_assert(Variable<'a', 'a'>{}.prev() == Variable<'z'>{});
+static_assert(Variable<'a', 'a'>{}.prev()      == Variable<'z'>{});
 static_assert(Variable<'a', 'a', 'a'>{}.prev() == Variable<'a', 'z'>{});
+
+
 
 } //namespace fol
 
