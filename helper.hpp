e845af604b9fcd21007ac7a2db84c1d04e29e809
Björn Schäpers <bjoern@hazardy.de>
2017-08-05 09:49:04 +0200

    Generalized tuple printer
    Now we can state the delimiter and it is not fixed to ", ". This is in
    preparation of And<...>.

--- helper.hpp
+++ helper.hpp
@@ -14,13 +14,18 @@ namespace fol {
 namespace details {
 
 template<typename Tuple, std::size_t I>
-std::ostream& print(std::ostream& os, const Tuple& t, const std::index_sequence<I>) {
+std::ostream& print(std::ostream& os, const Tuple& t, const char*, const std::index_sequence<I>) {
 	return os << std::get<I>(t);
 }
 template<typename Tuple, std::size_t I1, std::size_t I2, std::size_t... Is>
-std::ostream& print(std::ostream& os, const Tuple& t, const std::index_sequence<I1, I2, Is...>) {
-	os<<std::get<I1>(t)<<", ";
-	return print(os, t, std::index_sequence<I2, Is...>{});
+std::ostream& print(std::ostream& os, const Tuple& t, const char *delimiter, const std::index_sequence<I1, I2, Is...>) {
+	os<<std::get<I1>(t)<<delimiter;
+	return print(os, t, delimiter, std::index_sequence<I2, Is...>{});
+}
+
+template<typename... Ts>
+std::ostream& print(std::ostream& os, const std::tuple<Ts...>& t, const char *delimiter) {
+	return print(os, t, delimiter, std::index_sequence_for<Ts...>{});
 }
 
 }
@@ -32,7 +37,7 @@ std::ostream& operator<<(std::ostream& os, const std::tuple<T>& t) {
 
 template<typename... Ts>
 std::ostream& operator<<(std::ostream& os, const std::tuple<Ts...>& t) {
-	return details::print(os, t, std::index_sequence_for<Ts...>{});
+	return details::print(os, t, ", ", std::index_sequence_for<Ts...>{});
 }
 
 }
