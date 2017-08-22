18353dbecbe152363c6287ee640b80097be1f5dc
Björn Schäpers <bjoern@hazardy.de>
2017-08-22 09:39:29 +0200

    ConstexprVariant: Allow non trivial destructible types

--- constexpr_variant.hpp
+++ constexpr_variant.hpp
@@ -1,6 +1,8 @@
 /**
  * @file
  * @brief Contains a simple constexpr variant.
+ * 
+ * This implementation is based on GCC's std::variant implementation.
  */
 
 #ifndef CONSTEXPR_VARIANT_HPP
@@ -19,15 +21,61 @@ template<typename T, typename Head, typename... Tail>
 struct IndexOf<T, Head, Tail...> : std::integral_constant<size_t,
                                                           std::is_same_v<T, Head> ? 0 : IndexOfV<T, Tail...> + 1> { };
 
+template<typename Type, bool = std::is_literal_type_v<Type>>
+struct Uninitialized;
+
+template<typename Type>
+struct Uninitialized<Type, true> {
+	Type Storage;
+	
+	template<typename... Args>
+	constexpr Uninitialized(Args&&... args) : Storage(std::forward<Args>(args)...) {
+		return;
+	}
+	
+	constexpr const Type& get(void) const & { return Storage; }
+	
+	constexpr Type& get(void) & { return Storage; }
+	
+	constexpr const Type&& get(void) const && { return std::move(Storage); }
+	
+	constexpr Type&& get(void) && { return std::move(Storage); }
+};
+
+template<typename Type>
+struct Uninitialized<Type, false> {
+	template<typename... Args>
+	constexpr Uninitialized(Args&&... args) {
+		::new (&Storage) Type(std::forward<Args>(args)...);
+		return;
+	}
+	
+	const Type& get() const &
+	{ return *Storage._M_ptr(); }
+	
+	Type& get() &
+	{ return *Storage._M_ptr(); }
+	
+	const Type&& get() const &&
+	{ return std::move(*Storage._M_ptr()); }
+	
+	Type&& get() &&
+	{ return std::move(*Storage._M_ptr()); }
+	
+	unsigned char Storage[sizeof(Type)];
+};
+
 template<typename... Types>
 union VarUnion { };
 
 template<typename Head, typename... Tail>
 union VarUnion<Head, Tail...> {
-	Head First;
+	Uninitialized<Head> First;
 	VarUnion<Tail...> Rest;
 	
-	constexpr VarUnion(void) = default;
+	constexpr VarUnion(void) : Rest{} {
+		return;
+	}
 	
 	template<typename... Args>
 	constexpr VarUnion(const std::in_place_index_t<0>, Args&&... args) : First(std::forward<Args>(args)...) {
@@ -36,12 +84,14 @@ union VarUnion<Head, Tail...> {
 	
 	template<std::size_t I, typename... Args>
 	constexpr VarUnion(const std::in_place_index_t<I>, Args&&... args) :
-		Rest(std::in_place_index<I-1>, std::forward<Args>(args)...) { }
+			Rest(std::in_place_index<I-1>, std::forward<Args>(args)...) {
+		return;
+	}
 	
 	template<std::size_t I>
 	constexpr auto get(void) const {
 		return 0;
-}
+	}
 };
 
 template<std::size_t I, typename... Types>
@@ -68,20 +118,20 @@ class ConstexprVariant {
 		} //if constexpr ( sizeof...(Ts) == 0 )
 		else {
 			if ( index == 0 ) {
-				return u1.First == u2.First;
+				return u1.First.Storage == u2.First.Storage;
 			} //if ( index == 0 )
 			return compare(index - 1, u1.Rest, u2.Rest);
 		} //else -> if constexpr ( sizeof...(Ts) == 0 )
 	}
 	
 	public:
-	constexpr ConstexprVariant(void) : U() {
+	constexpr ConstexprVariant(void) noexcept : U{} {
 		return;
 	}
 	
 	template<typename T>
-	constexpr ConstexprVariant(T&& t) : U(std::in_place_index<details::IndexOfV<T, Types...>>, std::forward<T>(t)),
-	                                    Index(details::IndexOfV<T, Types...>) {
+	constexpr ConstexprVariant(T&& t) : U{std::in_place_index<details::IndexOfV<T, Types...>>, std::forward<T>(t)},
+	                                    Index{details::IndexOfV<T, Types...>} {
 		return;
 	}
 	
