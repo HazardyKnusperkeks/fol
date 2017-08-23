ce96895841f2c177727dc4f46bb76bed6d9804a6
Björn Schäpers <bjoern@hazardy.de>
2017-08-23 22:43:52 +0200

    ArraySet: Add contains()

--- array_set.hpp
+++ array_set.hpp
@@ -10,11 +10,14 @@
 #include "constexpr_variant.hpp"
 
 #include <array>
+#include <type_traits>
 
 template<std::size_t N, typename... Types>
 class ArraySet {
 	std::array<ConstexprVariant<Types...>, N> Data;
 	std::size_t Index = 0;
+	const decltype(Data.begin()) Begin = Data.begin();
+	decltype(Data.begin()) End = Data.begin();
 	
 	public:
 	constexpr ArraySet(void) = default;
@@ -22,7 +25,13 @@ class ArraySet {
 	template<typename... Args>
 	constexpr ArraySet(Args&&... args) : Data{std::forward<Args>(args)...}, Index{sizeof...(Args)} {
 		static_assert(sizeof...(Args) <= N, "More initializers than elements in the set allowed!");
+		constexprAlgo::advance(End, static_cast<std::make_signed_t<decltype(Index)>>(Index));
 		return;
+	}
+	
+	template<typename Type, std::enable_if_t<(std::is_same_v<Type, Types> || ...)>* = nullptr>
+	constexpr bool contains(const Type& t) const noexcept {
+		return constexprAlgo::find(Begin, End, t) != End;
 	}
 	
 	template<std::size_t N2>
@@ -40,6 +49,13 @@ namespace tests {
 using SetType = ArraySet<10, int, bool, char>;
 
 static_assert(SetType{} == SetType{});
+static_assert(SetType{5, true, 7, 'h'}.contains(true));
+static_assert(SetType{5, true, 7, 'h'}.contains('h'));
+static_assert(SetType{5, true, 7, 'h'}.contains(5));
+static_assert(!SetType{5, true, 7, 'h'}.contains(false));
+static_assert(!SetType{5, true, 7, 'h'}.contains(6));
+static_assert(!SetType{5, true, 7, 'h'}.contains('c'));
+
 }
 
 #endif
